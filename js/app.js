6dedc9702fb60f76de254941ec8355dfbfb4ad0a
giftess.in <igiftess@gmail.com>
2026-02-19 04:38:58 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
+import { auth } from "./firebase.js";
+import { signInWithEmailAndPassword } 
+from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
+const loginBtn = document.getElementById("loginBtn");
+
+if (loginBtn) {
+  loginBtn.addEventListener("click", async () => {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const message = document.getElementById("authMessage");
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      message.style.color = "green";
+      message.innerText = "Login successful!";
+      
+      setTimeout(() => {
+        window.location.href = "index.html";
+      }, 1000);
+
+    } catch (error) {
+      message.style.color = "red";
+      message.innerText = error.message;
+    }
+  });
+}
