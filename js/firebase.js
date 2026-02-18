600857cc6139a77ea4dea488d6777cc700dc9d64
giftess.in <igiftess@gmail.com>
2026-02-19 04:50:27 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,20 +1,16 @@
-// Import Firebase (using CDN)
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
-// Your Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyCsZJviWYXZ-1Buclavnkk6P3gU43fCmtM",
   authDomain: "giftess-1119.firebaseapp.com",
   projectId: "giftess-1119",
   storageBucket: "giftess-1119.firebasestorage.app",
   messagingSenderId: "455652729219",
-  appId: "1:455652729219:web:ce4cc662b1955a105796b6",
+  appId: "1:455652729219:web:ce4cc662b1955a105796b6"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
