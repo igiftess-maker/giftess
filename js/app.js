2a3022204797a8e9c4a732fdc4a11e34bd0df179
giftess.in <igiftess@gmail.com>
2026-02-20 02:41:39 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,33 @@
+import { db } from "./firebase.js";
+import { collection, getDocs } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
+const productContainer = document.getElementById("productContainer");
+
+if (productContainer) {
+  const loadProducts = async () => {
+    try {
+      const querySnapshot = await getDocs(collection(db, "products"));
+      productContainer.innerHTML = "";
+
+      querySnapshot.forEach((doc) => {
+        const product = doc.data();
+
+        productContainer.innerHTML += `
+          <div class="product-card">
+            <img src="${product.image}" alt="${product.name}">
+            <div class="product-info">
+              <h3>${product.name}</h3>
+              <p>${product.description}</p>
+              <div class="product-price">₹${product.price}</div>
+            </div>
+          </div>
+        `;
+      });
+
+    } catch (error) {
+      console.error("Error loading products:", error);
+    }
+  };
+
+  loadProducts();
+}
